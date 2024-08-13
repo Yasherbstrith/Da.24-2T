3460176ee78004ccf2b8bbce72fa66ce55f3b3af
Yasherbstrith <yas.valaneiraherbstrith1@gmail.com>
2024-08-13 17:02:31 -0300

    Add files via upload

---
+++ aula 3/index.js
@@ -0,0 +1,51 @@
+// 
+console.log("olá mundo");
+//prompt("qual seu nome")
+
+
+const nome= "Yasmin"
+const idade= 15
+const escola= "Cursando"
+
+
+console.log ("olá","meu nome é:", nome, "e eu tenho:",idade, "anos")
+console.log ("nome: Yasmin")
+console.log ("sobrenome: Herbstrith")
+console.log ("escola: cursando")
+console.log ("idade:15 anos")
+
+
+
+const got= "game of thrones"
+const temporadaDeGot= "8;"
+
+console.log(typeof got)
+console.log (typeof temporadaDeGot)
+
+
+let novaVariavel
+console.log (typeof novaVariavel)
+
+novaVariavel= 2 
+console.log (typeof novaVariavel)
+
+novaVariavel= undefined
+console.log (typeof novaVariavel)
+
+
+//let a =prompt("qual é seu nome?")
+//console.log (a)
+
+//let b =prompt ("qual sua idade?")
+//console.log  (b)
+
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+
+const idadeTexto2 = "23"
+const idadeNumero2 = Number(idadeTexto)
+console.log(typeof idadeTexto)
+console.log(typeof idadeNumero)
+
