cff7b86618c2649efa9ac9d154458b1776b52db5
Yasherbstrith <yas.valaneiraherbstrith1@gmail.com>
2024-08-20 17:21:37 -0300

    Add files via upload

---
+++ aula 6/index.js
@@ -0,0 +1,35 @@
+//const primeiroValor = 10
+//const segundoValor = 20
+
+//const resultado = primeiroValor + segundoValor
+
+//console.log(resultado)
+
+//const resultado = primeiroValor - segundoValor
+
+//console.log(resultado)
+
+//const resultado = primeiroValor * segundoValor
+
+//console.log(resultado)
+
+//const resultado = primeiroValor / segundoValor
+
+//console.log(resultado)
+
+// const resultado = primeiroValor % segundoValor
+
+//console.log(resultado)
+
+/*let resultado = 100
+
+resultado = resultado +10
+console.log(resultado)
+
+let resultado1 = 100
+resultado + = 10
+
+console.log(resultado1)*/
+
+const condicao = 1 !== 1 
+console.log(condicao)
---
+++ aula 7/index.js
@@ -0,0 +1,112 @@
+/*const primeiroValor = true
+const segundoValor = true 
+
+const resultado = primeiroValor&& segundoValor
+
+console.log(resultado)*/
+
+/*const a = false
+const b = true
+const c = false*/
+
+//const resultado = a && b
+
+//console.log(resultado)
+
+//const resultado = b && c
+
+//console.log(resultado)
+
+//const resultado = a && c
+
+//console.log(resultado)
+
+//const resultado = a && b && c
+
+//console.log(resultado)
+
+/*const primeiroValor = true
+const segundoValor = true
+
+const resultado = primeiroValor || segundoValor
+console.log(resultado)*/
+
+/*const a = true 
+const b = false 
+const c = true*/ 
+
+//const resultado = a || b
+//console.log(resultado)
+
+//const resultado = b || c 
+//console.log(resultado)
+
+//const resultado = a || c 
+//console.log(resultado)
+
+//const resultado = a || b || c
+//console.log(resultado)
+
+//const primeiroValor = true
+//const segundoValor = !primeiroValor
+
+//console.log(resultado)
+
+
+/*const nome = prompt("qual seu nome?")
+console.log("nome:", nome)
+
+const anoNasci = prompt("qual o seu ano de nascimento?")
+
+const anoAtual = prompt("qual o ano atual?")
+
+const idade = anoAtual - anoNasci
+console.log("idade:", idade)
+
+const maiorIdade = idade>=18
+console.log("é maior de idade?", maiorIdade )
+
+const idadeEm2050 = 2050 - anoAtual
+console.log("Idade em 2050",idade+idadeEm2050)*/
+
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("c.", resultado)
+
+resultado = !resultado && (bool1 || bool2)
+console.log("b.", resultado)
+
+resultado = !resultado && (bool1 || bool2)
+console.log("c.", resultado)
+
+console("d.", typeof resultado)*/
+
+
+/*let primeiroNumero = prompt("digite um numero!")
+let segundoNumero = prompt("digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+console.log(soma) 
+
+// o problema é que não tem uma "conta" definida, como por exemplo:
+
+const primeiroNumero = 35
+const segundoNumero = 7746
+
+const soma = primeiroNumero + segundoNumero
+console.log(soma)*/
+
+const idade = prompt("qual sua idade?")
+console.log("idade:", idade)
+
+const idadeAmigo = prompt("qual a idade do seu melhor amigo?")
+console.log("idadeAmigo:", idadeAmigo)
+
+const maisVelho = idade > idadeAmigo
+console.log("sua idade é maior do que a do seu melhor amigo:", maisVelho)
+
+console.log( typeof a diferença de idade é de 21 anos)
+
