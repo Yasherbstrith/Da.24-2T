3507ee1ee4ea44a02e0ce3bceadaa6c31979ae9d
Yasherbstrith <yas.valaneiraherbstrith1@gmail.com>
2024-08-22 17:15:46 -0300

    Add files via upload

---
+++ aula 8/index.js
@@ -0,0 +1,88 @@
+/*
+//Exemplo concatenação:
+/*const texto1 = "Olá mundo"
+const texto2 = 'Olá mundo'
+const texto3 = 'Olá mundo'
+
+const nome = Yasmin
+const idade = 15
+
+const frase = "O meu nome é: " + nome + " e minha idade é:" + idade + " anos."
+
+console.log(frase)
+
+/*
+//Exemplo template:
+const frase = Meu nome é: ${nome} e minha idade é: ${idade} anos
+
+console.log(frase)*/
+
+/*const nomePessoa = prompt("qual o seu nome?")
+const corFav = prompt("qual sua cor favorita?")
+console.log("nome:", nomePessoa, "cor:", corFav)
+*/
+
+/*const nome = `Yasmin`
+console.log(nome.length)
+
+console.log(nome.toLowerCase())
+
+console.log(nome.toUpperCase())
+
+const email = "yas.valaneiraherbstrith1@gmail.com"
+console.log(email.trim())
+
+const frase "hoje comi cenoura"
+console.log(frase.includes("cenoura"))
+
+console.log(frase.includes("batatas"))
+
+const frase = "hoje comi cenoura, adoro cenoura."
+
+const frase2 = frase.replaceAll("cenoura", "batata")
+
+console.log(frase2)
+
+const frase = "amo ler."
+console.log(frase.toUpperCase())
+
+const frase2 = frase.replaceAll("o", "i")
+console.log(frase2)
+
+console.log(frase2.length)
+
+const frutas =["abacate", "banana", "tomate","milho", "uva", "morango"]
+
+const fruta = frutas[4]
+console.log(fruta)
+
+
+const racasCachorros = ["Shih Tzu","Rottweiler", "Pug", "Golden Retriever","Pastor Alemão"]
+const escolhido = racasCachorros [2]
+
+console.log(escolhido)
+
+const pokemons = [" Bulbasaur", "Charmander", "Wartortle", "Ivysaur"]
+console.log(pokemons.length)
+
+const seriesFav = ["supernatural", "the diaries vampire"]
+console.log(seriesFav.includes("supernatural"))
+console.log(seriesFav.includes("one the hill"))
+
+const numeros = [1,2,3]
+
+numeros.push(4)
+
+console.log(numeros)
+
+
+numeros.push(5,6,7,8)
+console.log(numeros)*/
+
+const meusPeixes = ["palhaço", "mandarim", "estorjão"]
+
+console.log(meusPeixes)
+
+meusPeixes.pop()
+
+console.log(meusPeixes)
