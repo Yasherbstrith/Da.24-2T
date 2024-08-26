c374fac53643593b89a557ba1839977d60f57721
Yasherbstrith <yas.valaneiraherbstrith1@gmail.com>
2024-08-26 17:11:26 -0300

    Add files via upload

---
+++ aula 9/index.js
@@ -0,0 +1,51 @@
+/*let array
+console.log('a.',array)
+//define um nome para a variavel
+
+array = null
+console.log('b.',array)
+// definiu o null como um valor
+
+array = [3,4,5,6,7,8,9,10,11,12,13]
+console.log('c.',array.lenght)
+//esta definindo o tamanho da variavel
+
+let i = 0
+console.log('d.',array[i])
+//esta substituindo o nome da variavel
+
+array[i+1]=19
+console.log('e.',array)
+//esta substituindo o valor de i para 19*
+
+const valor = array[i+6]
+console.log('f.',valor)
+//esta definindo o valor e o nome da variavel
+
+const frase = prompt("digite uma frase")
+// definiu a variavel e deu o comando para fazer uma pergunta no console
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+// comando para imprimir, botar a frase em letras maiusculas, substituir o a* pelo i* e contar os caracteres que consta na frase.
+
+const nome = prompt("qual o seu nome?")
+const email = prompt("qual o seu email?")
+console.log(nome)
+console.log(email)
+
+alert(O e-mail ${emailDoUsuario}, foi cadastrado com sucesso. Seja bem-vindo(a), ${nomeDoUsuario}!)
+
+const comidasFav = ["batata frita com sardinha", "hamburguer", "strogonoff", "pastel", "pizza"]
+console.log(comidasFav)
+
+console.log ("essas sao minhas comidas favortas" , comidasFav)
+
+const comidas = prompt("qual sua comida favorita")
+
+comidas.splice(1,4) 
+
+comidasFav.push(comidas, "hamburguer", "strogonoff", "pastel", "pizza")
+console.log(comidasfav)*/
+
+const listaDeTarefa = []
+const suaLista = prompt("qual sua lista de tarefa?")
+const 
