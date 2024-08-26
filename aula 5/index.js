6ca375b88da11bbf46fbf58282e5792a79888929
Yasherbstrith <yas.valaneiraherbstrith1@gmail.com>
2024-08-26 16:47:59 -0300

    Add files via upload

---
+++ aula 5/index.js
@@ -0,0 +1,25 @@
+const primeiroValor = 4
+const segundoValor = 5
+
+const resultado = primeiroValor + segundoValor
+console.log(resultado)
+
+const multiN = 3
+const multiNu = 5
+const diviN = 2
+
+const soma = multiN * multiNu % diviN 
+console.log(soma)
+
+const terceiroValor = 5
+const quartoValor = 4
+const quintoValor = -1
+
+const resposta = terceiroValor - quartoValor * quintoValor
+console.log(resposta)
+
+const sextoValor = 234
+const setimoValor = 5
+
+const resultados = sextoValor % setimoValor
+console.log(resultados)
