9910d40b868a448e63e56c3ff672d8fd8eeae2fa
Yasherbstrith <yas.valaneiraherbstrith1@gmail.com>
2024-09-09 17:09:27 -0300

    Add files via upload

---
+++ aula 15/index.js
@@ -0,0 +1,124 @@
+/*function calculaArea(largura, altura){
+    const area = largura * altura
+console.log(area)
+}
+
+calculaArea(2,3)
+calculaArea(5,4)
+calculaArea(20,4)
+calculaArea(8,6)
+
+function imprimirOlaMundo(){
+    console.log("Olá mundo")
+}
+imprimirOlaMundo()
+imprimirOlaMundo()
+imprimirOlaMundo()
+
+function imprimirNome(nome){
+    console.log("bem vinda", nome)
+}
+imprimirNome("Bruna")
+
+function imprimirTurma(){
+    console.log("Bem vindos turma 24-2T!")
+}
+imprimirNome()
+
+function calcular(num1, num2){
+    const soma = num1 + num2
+console.log("a soma é:", soma) // esse
+}
+calcular(25,65)
+
+function numeroDobre(num3, num4){
+    const resultado = num3 * num4
+    console.log("o resultado é:", resultado)
+}
+numeroDobre(25,2)//
+
+function consoleNome(nome){
+    console.log(`Olá ${nome}`)
+}
+consoleNome("Luana")
+consoleNome("Amanda")
+consoleNome("Eduarda")
+
+function doisNumeros(num5, num6){
+    const conta = num5 + num6
+    console.log(`A soma é ${conta}`)
+}
+doisNumeros(86,46)
+doisNumeros(46, 97)
+
+function mensagemA(){
+    console.log("Olá Lucas, você parece um colega bem inteligente")
+}
+mensagemA()
+
+function calculaArea(largura, altura){
+    const resultadoArea = largura * altura
+    return resultadoArea
+}
+
+let area = calculaArea(2,5)
+console.log("a area é:", area)
+
+console.log("a area é:", calculaArea(2,5))
+
+
+function calcularNum(num7, num8){
+    const numCalcular = num7 + num8
+    return numCalcular
+}
+let resultado =calcularNum(3,7)
+console.log("o resultado é:", resultado)
+
+const numeros = [ 278, 635, 863, 816, 572, 98]
+
+function primeiroeUltimo(arrayNumeros){
+    const novoArray = []
+    novoArray.push(arrayNumeros[0])
+    novoArray.push(arrayNumeros[arrayNumeros.length -1])
+    return novoArray
+}
+
+console.log(primeiroeUltimo(numerosOriginal))
+
+const calculaArea = function(largura, altura){
+    const soma = largura * altura
+    return soma
+}
+
+const resultado = calculaArea(2,5)
+
+function imprimirNome(nome){
+    console.log("bem vinda", nome)
+}
+imprimirNome("Bruna")
+imprimirNome("Kauã")
+
+const imprimirNome = function(nome){
+    console.log("bem vindo", nome)
+}
+imprimirNome("Bruna")
+
+const calculaArea = (largura, altura)=> {
+    const resultado = largura * altura
+    return resultado 
+}
+const area = calculaArea(2,7)
+console.log("a area é:", area)*/
+
+
+const soma = (num1, num2) => {
+    resultado = num1 + num2 
+}
+soma(25,65)
+console.log("a soma é:", resultado)
+
+const calcular = (num3, num4) => {
+    resultado = num3 * num4
+}
+calcular(64,2)
+console.log("o dobro de 64 é:", resultado)
