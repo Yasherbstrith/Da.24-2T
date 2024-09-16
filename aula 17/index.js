1020cdd520df808d901c875b138299646328a106
Yasherbstrith <yas.valaneiraherbstrith1@gmail.com>
2024-09-16 17:14:46 -0300

    Add files via upload

---
+++ aula 17/index.js
@@ -0,0 +1,126 @@
+/*const professor = {
+    nome: "Lucas",
+    idade: 32,
+    altura: 1.75,
+    email: 'lucas@gmail.com',
+    tarefas: ['dar aula', 'responder dúvidas', 'ler texto'],
+    contarPiadas: function(){
+        console.log("é pa vê ou pa comê?")
+    }
+}
+
+console.log(professor.nome)
+console.log(professor.idade)
+console.log(professor.altura)
+console.log(professor['email'])
+
+professor.nome = 'Leo'
+professor["email"] = 'leo@gmail.com'
+console.log(professor.nome)
+console.log(professor['email'])*/
+
+/*const filme = {
+    direcao: 'Christian Ditter',
+    nome: 'Simplesmente Acontece',
+    ano: 2014,
+    elenco: ['Lily Collins', 'Sam Claflin', 'Christian Cooke', 'Jaime Winstone'],
+    propriedadesF: function(){
+        console.log("eu já assisti")
+    }
+} 
+console.log(filme.direcao)
+console.log(filme.nome)
+console.log(filme['ano'])
+console.log(filme['elenco'])
+console.log("eu já assisti")*/
+
+/*const pessoa = {
+    nome: "Yasmin",
+    idade: 16,
+    genero: "pop"
+}
+console.log("o nome dela é", pessoa.nome, "ela tem", pessoa.idade, "anos e ela gosta de", pessoa.genero)*/
+
+/*const donoDoPet = {
+    nome: 'Yasmin',
+    idade: 16,
+    pet:{
+        nome:'Lia',
+        raca:'vira-lata',
+        idade: 5
+    }
+}
+console.log(donoDoPet.pet.nome)*/
+
+/*const curso = {
+    nome: "noturno Frontend",
+    linguagens: ["js", "css", "html"]
+}
+console.log(curso.linguagens[1])*/
+
+/*const professores = [
+    {nome: "Lucas", modulo: 1},
+    {nome: "Leo", modulo: 2},
+    {nome: "Mika", modulo: 3}
+]
+console.log(professores)
+console.log(professores[0].nome)*/
+
+/*const curso = {
+    nome: "noturno Frontend",
+    linguagens: ["js", "css", "html"]
+}
+console.log(curso.linguagens[1])
+
+curso.numeroDeEstudante = 50 
+curso['numerosDeProfessores'] = 10 
+
+console.log(curso.numeroDeEstudante)
+console.log(curso.numerosDeProfessores)*/
+
+/*const filme = {
+    personagens: ['Rosie', 'Alex', 'Greg', 'Ruby'],
+    elenco: ['Lily Collins', 'Sam Claflin', 'Christian Cooke', 'Jaime Winstone'],
+} 
+filme.elenco[0] = 'Xuxa'
+console.log(filme.elenco)*/
+
+/*const usuario = {
+    nome: 'Prof',
+    idade: 25,
+    email: 'prof@senac.com.br',
+    cidade: 'são leopoldo'
+}
+
+const novoUsu = {
+    ...usuario,
+    nome:'Lucas',
+    idade: 32,
+    cidade: 'Canoas',
+}
+console.log(novoUsu)*/
+
+/*const nomeProfs = ['Lucas', 'Leo', 'Mika']
+
+const novaLista = [...nomeProfs]
+
+console.log(novaLista)*/
+
+
+/*let pessoa = {
+    nome: "Yasmin",
+    comidasFav: ["strogonoff", "escondidinho", "alaminuta"]
+    
+}
+
+const melhorAmg = [...pessoa [nome1 = 'Bruna'], [idade2 = 16]]
+
+console.log("o meu nome é",nome, "e minhas comidas favoritas são", comidasFav, "minha melhor amiga se chma", nome1, "ela tem", idade2, "anos" )*/
+
+// Sete Pecados Capitais
+
+const personagens = [
+    {nome: 'Diane', idade: 'Aproximadamente 750 anos', classe: 'Gigantes', poder: 'Creation'}
+    {nome: 'Meliodas', idade: ' mais de 3.000 anos', classe: 'Demônio', poderes: 'Hellblaze, Full Counter' }
+    {nome: 'Escanor', idade: '40 anos', classe: 'Humano', poder: 'Sushine'}
+]// atividade que n deu tempo de terminar.
